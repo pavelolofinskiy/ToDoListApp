79c00319c15107f2966d3a768db2010bb892b599
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-05-31 13:32:57 +0300

    set up search

--- base/views.py
+++ base/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth import login
 
 from .models import Task
 
+
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
@@ -45,7 +46,15 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__startswith=search_input)
+
+        context['search_input'] = search_input
+
         return context
 
 
@@ -58,7 +67,7 @@ class TaskDetail(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = ['title', 'description', 'complete']
-    success_url = reverse_lazy()
+    success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
